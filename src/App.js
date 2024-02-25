fa1bc9576a4d3fc260c9b559f14d97bbae321eb8
Shivam8999 <king8999274125@gmail.com>
2024-02-25 12:03:45 +0530

    Feat: Change color useState hook and onChnage functions are used to change the colors based on dropdown selection

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import './App.css';
 import './css/tailwind.css'
 import Resume from './components/Resume';
 import person from './data/Person'
+import { useState } from 'react';
 
 function App() {
+  const [newColor, setnewColor] = useState('text-black');
+  function oncolorChange(event) {
+    setnewColor(document.getElementById('colors').value);
+
+  }
   return (
-    <div className="App h-[100vh]">
-      <div className="h-7 w-full bg-white relative" />
-      <div className='grid grid-cols-[1.75rem_auto_1.75rem] h-[95%]'>
-        <div className="bg-white w-7 h-full"></div>
-        <div className='flex justify-center'><Resume fName={person[0].fName} lName={person[0].lName} email={person[0].email} cNumber={person[0].cNumber} desc={person[0].desc} edu={person[0].edu} exp={person[0].exp} skills={person[0].skills} /></div>
-        <div className="bg-white w-7 h-full"></div>
+
+    <div div className="App h-[100vh]" >
+      {console.log('app render')}
+      <div className='grid lg:grid-cols-[0%_80%_20%] grid-row-[70%_10%_20%] h-[95%]'>
+        <div className="bg-white h-full"></div>
+        <div className='flex justify-center'><Resume fName={person[0].fName.toString()} lName={person[0].lName.toString()} email={person[0].email.toString()} cNumber={person[0].cNumber.toString()} desc={person[0].desc.toString()} edu={person[0].edu} exp={person[0].exp} skills={person[0].skills} color={newColor.toString()} /></div>
+        <div className="bg-white h-full">
+          <label htmlFor="colors">Choose a title color:</label>
+          <select name="colors" id="colors" onChange={oncolorChange}>
+            <option value="text-slate-500">Gray</option>
+            <option value="text-black" selected>Black</option>
+            <option value="text-red-600">Red</option>
+            <option value="text-blue-600">Blue</option>
+          </select>
+        </div>
       </div>
-      {console.log(person[0])}
+      {/* {console.log(person[0])} */}
 
       {/* <div className="h-7 w-full bg-red-300 absolute bottom-0" /> */}
-    </div>
+    </div >
   );
 }
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Resume({ fName, lName, email, cNumber, desc, edu, exp, skills }) {
+function Resume({ fName, lName, email, cNumber, desc, edu, exp, skills, color = 'text-slate-600' }) {
+    // color = 'text-red-600';
+    // if (color==){}
     return (
         <>
-            <div className='w-full max-w-5xl border-2 justify-items-start px-16 py-8 text'>
-                <div className='flex justify-between'><h1 className='text-start text-6xl font-medium w-min'>{fName} </h1><div className='self-end'>{email}</div></div>
+            <div className='w-full max-w-5xl border-2 justify-items-start sm:px-16 py-8 px-6 '>
                 <div className='flex justify-between'>
-                    <h1 className='text-start text-6xl font-medium w-min'>{lName}</h1>
-                    <div className='self-start'>{cNumber}</div>
+                    <h1 className={`text-start md:text-6xl text-4xl font-medium w-min ${color}`}>{fName} </h1>
+                    <div className={`self-end text-sm md:text-base ${color}`}>{email}</div></div>
+                <div className='flex justify-between'>
+                    <h1 className={`text-start md:text-6xl text-4xl font-medium w-min ${color}`}>{lName}</h1>
+                    <div className={`self-start text-sm md:text-base ${color}`}>{cNumber}</div>
                 </div>
                 <hr />
-                <div className='text-start py-4'>{desc}
+                <div className='text-start py-4 text-sm md:text-base'>{desc}
                 </div>
                 <hr />
                 <div className='text-start my-4'>
-                    <div className='text-2xl font-medium mb-2'>Education</div>
+                    <div className={`md:text-2xl text-xl font-medium mb-2 ${color}`}>Education</div>
                     {edu.map((details) => {
                         return (
-                            <div className='flex justify-between mt-2'>
-                                <div className=''>{details.courseName} | {details.collegeName}</div>
+                            <div className='flex justify-between mt-2 text-sm md:text-base'>
+                                <div className='max-w-48 sm:max-w-max'>{details.courseName} | {details.collegeName}</div>
                                 <div className='flex justify-between'>
                                     <span className='mx-3'>{details.passPercent}%</span>
                                     <span>{details.startYear}-{details.endYear}</span>
@@ -28,25 +32,25 @@ function Resume({ fName, lName, email, cNumber, desc, edu, exp, skills }) {
                 </div>
                 <div className='text-start my-4'>
 
-                    <div className='text-2xl font-medium'>Experience</div>
+                    <div className={`md:text-2xl text-xl font-medium ${color}`}>Experience</div>
                     {
                         exp.map((ex) => {
                             return (<>
-                                <div className='flex justify-between mt-2'>
+                                <div className='flex justify-between mt-2 text-sm md:text-base font-semibold'>
                                     <div className='mb-1'>{ex.role} | {ex.org}</div>
                                     <div className='flex justify-between'>
                                         <span>{ex.startMonth} {ex.startYear}- {ex.endMonth} {ex.endYear}</span>
                                     </div>
                                 </div>
-                                <div>
+                                <div className='text-sm md:text-base'>
                                     {ex.desc}
                                 </div>
                             </>);
                         })}
                 </div>
                 <div className='text-start my-4'>
-                    <div className='text-2xl font-medium mb-2 grid-cols-[auto_auto_auto]'>Skills</div>
-                    <div className='grid grid-cols-3 list-disc mx-5'>
+                    <div className={`md:text-2xl text-xl font-medium mb-2 grid-cols-[auto_auto_auto] ${color}`}>Skills</div>
+                    <div className='grid grid-cols-3 list-disc mx-5 text-sm md:text-base'>
                         {
                             skills.map((skill) => { return <li>{skill}</li> })
                         }
